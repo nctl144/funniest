f7bddd047b379765159ce7119a7aed875a66a03c
nctl144 <nctl144@gmail.com>
2018-02-15 14:38:36 -0500

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+    version='0.1',
+    description='The funniest joke in the world',
+    url='https://github.com/nctl144/funniest',
+    author='chau tung lam nguyen',
+    author_email='nctl144@gmail.com',
+    license='MIT',
+    packages=['funniest'],
+    zip_safe=False)
